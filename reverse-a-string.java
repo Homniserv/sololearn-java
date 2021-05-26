36ec9fa4d5c357dfb55df1452c72918b814ce764
moueza <221372+moueza@users.noreply.github.com>
2021-05-26 16:31:41 +0200

    See #1 gen OK

--- reverse-a-string.java
+++ reverse-a-string.java
@@ -1,7 +1,7 @@
 https://www.sololearn.com/learning/eom-project/1068/950
 
 import java.util.Scanner;
-
+ 
 public class Program
 {
 	public static void main(String[] args) {
@@ -9,13 +9,24 @@ public class Program
 		String text = scanner.nextLine();
 		char[] arr = text.toCharArray();
 
-		int l =arr.length();
-		char[] res=char[l];
-		for(int i=l-1;i<=0;i--){
-			int resI=(l-1)-i;
-			//your code goes here
-			res[resI]=arr[i];
-        }
-		System.out.println(res);
+		 
+		int l = arr.length;
+		char[] res = new char[l];
+		String chn = "";
+		for (int i = l - 1; i >= 0; i--) {
+			int resI = (l - 1) - i;
+			//System.out.println("i=" + i);
+			// your code goes here
+			String s = Character.toString(arr[i]);
+			//System.out.println("char=" + arr[i]);
+
+			// System.out.println("s=" + s);
+			chn = chn + s;
+			// res[resI]=arr[i];
+			// chn[resI]=arr[i];
+			//System.out.println("OK");
+		}
+		System.out.println(chn);
+		//System.out.println("fin");
 	}
 }

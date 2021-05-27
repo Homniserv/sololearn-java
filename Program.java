d03b2aff7238693641ab3e8f23328bbd07647993
moueza <>
2021-05-27 07:04:05 +0200

    sov

---
+++ Program.java
@@ -0,0 +1,39 @@
+import java.util.*; 
+
+public class Bowling {
+    HashMap<String, Integer> players;
+    Bowling() {
+        players = new HashMap<String, Integer>();
+    }
+    public void addPlayer(String name, int p) {
+        players.put(name, p);
+    }
+    //your code goes here
+    public String getWinner() {
+        Iterator it =new players.iterator();
+        int maxi=-1:
+        while(it.hasNext()){
+            int val=it.next().getValue();
+            if(val>maxi)
+                maxi=val;
+        }
+        System.out.println();
+    }
+    
+}
+
+public class Program {
+    public static void main(String[ ] args) {
+        Bowling game = new Bowling();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<3;i++) {
+            String input = sc.nextLine();
+            String[] values = input.split(" ");
+            String name = values[0];
+            int points = Integer.parseInt(values[1]);
+            game.addPlayer(name, points);
+        }
+        game.getWinner();
+    }
+}

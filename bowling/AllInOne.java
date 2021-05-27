b1c3a826f11e5cf6dcc5fc4b2d4215ae7877ff29
moueza <>
2021-05-27 08:25:06 +0200

    see #1 gen OK

--- bowling/AllInOne.java
+++ bowling/AllInOne.java
@@ -1,12 +1,21 @@
-package bowling;
+import java.util.*; 
 
 import java.util.HashMap;
 import java.util.function.BiConsumer;
 
 public class Bowling {
-	int maxi = -1;
-
-	/**
+    HashMap<String, Integer> players;
+    Bowling() {
+        players = new HashMap<String, Integer>();
+    }
+    public void addPlayer(String name, int p) {
+        players.put(name, p);
+    }
+    //your code goes here
+    int maxi = -1;
+    String kk;
+    int vv;
+    /**
 	 * Defining Our Action in MyBiConsumer class
 	 * https://www.geeksforgeeks.org/hashmap-foreachbiconsumer-method-in-java-with-examples/#:~:text=The%20forEach(BiConsumer)%20method%20of,in%20the%20order%20of%20iteration.
 	 */
@@ -17,24 +26,15 @@ public class Bowling {
 			 * System.out.print("Key = " + k); System.out.print("\t Value = " + v);
 			 * System.out.println();
 			 */
-			if (maxi < v)
+			if (maxi < v){
 				maxi = v;
+                kk=k;
+                vv=v;
+            }
 		}
 
 	}
-
-	HashMap<String, Integer> players;
-
-	Bowling() {
-		players = new HashMap<String, Integer>();
-	}
-
-	public void addPlayer(String name, int p) {
-		players.put(name, p);
-	}
-
-	// your code goes here
-	public void getWinner() {
+    public void getWinner() {
 		// Iterator<Object> it = players.iterator();
 
 		// players.get(key)
@@ -55,10 +55,23 @@ public class Bowling {
 		 * while (it.hasNext()) { int val = it.next().getValue(); if (val > maxi) maxi =
 		 * val; }
 		 */
-		//System.out.println(players.get(key));
 		System.out.println(kk);
-		
-
 	}
+    
+}
 
+public class Program {
+    public static void main(String[ ] args) {
+        Bowling game = new Bowling();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<3;i++) {
+            String input = sc.nextLine();
+            String[] values = input.split(" ");
+            String name = values[0];
+            int points = Integer.parseInt(values[1]);
+            game.addPlayer(name, points);
+        }
+        game.getWinner();
+    }
 }
